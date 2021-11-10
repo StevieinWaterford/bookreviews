d54c4f4f6ed14ac1217bf823616349b7b6b7e2ba
StevieinWaterford <stevieindub@gmail.com>
2021-11-10 19:32:36 +0000

    Add deployment

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ["DATABASE_URL"] = "postgres://rynineflpwudfb:23e1438581d98eb9008d155df02929f5943f596bd637b3cc546873a161d7e101@ec2-54-171-25-232.eu-west-1.compute.amazonaws.com:5432/d6rrmn9ta9lio6"
 os.environ["SECRET_KEY"] = "randomSecretKeyAlan2021"
+os.environ["CLOUDINARY_URL"] = "cloudinary://884173494267683:2A1aK79n30uxfpSjxtgcjAXpt_g@dzbad1rgb"
